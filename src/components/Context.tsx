f00a8fbf9aeafade4774e7ccba5896d107602730
Nsilou Balandamio <71090230+nsiloub@users.noreply.github.com>
2024-11-12 14:08:02 +0000

    CLEARING: IGNORE IT

--- src/components/Context.tsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-type ImageSize = 100 | 150;
-
-export const ImageSizeContext = createContext<ImageSize>(100);
--- src/helpers/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Place } from "../assets/data";
-
-export function getImageUrl(place: Place): string {
-    return (
-        "https://i.imgur.com/" + place.imageId + "1.jpg"
-    )
-};
